29f8796bf59cc849ce19bef1f3055c19da516a8d
6ichem <likemysleep@gmail.com>
2022-12-05 15:09:02 +0100

    fix: space measurement fixes, footer

--- src/plaitv/Home/index.tsx
+++ src/plaitv/Home/index.tsx
@@ -6,6 +6,9 @@ export default function Home() {
     <div>
       <Navbar />
       <Hero />
+      <footer className="mt-auto text-quaternary font-semibold text-xs p-8 md:p-12">
+        Plai.tv, all rights reserved.
+      </footer>
     </div>
   );
 }
